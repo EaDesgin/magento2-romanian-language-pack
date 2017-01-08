d07d379d1b52fe07fa140a2cdc6dc47fea14e73a
eadesignro <office@eadesign.ro>
2017-01-08 21:25:44 +0200

    Added the language pack and a test section

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2017 EaDesign by Eco Active S.R.L. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'eadesigndev_ro_ro',
+    __DIR__
+);
